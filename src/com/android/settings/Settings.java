142a0ea3b248d4cd6c3d51301f817b69b16727b6
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Allow to quickly open Running Services from meminfo view
    neobuddy89: Updated for A12.
    
    Change-Id: I1abeb1bb799175083b3c81289f0a9679e92f1b31

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -488,4 +488,6 @@ public class Settings extends SettingsActivity {
 
     // BaseCamp Additoon
     public static class BaseCampActivity extends SettingsActivity {}
+
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
