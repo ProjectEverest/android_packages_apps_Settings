dc02a902abe0afc65415f7e9a99506cc4ea15b6c
maxwen <max.weninger@gmail.com>
2023-11-30 07:17:44 +0100

    Settings: Add BaseCamp to dashboard
    Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -521,4 +521,7 @@ public class Settings extends SettingsActivity {
      * Activity for PreviouslyConnectedDeviceDashboardFragment
      */
     public static class PreviouslyConnectedDevicesActivity extends SettingsActivity { /* empty */ }
+
+    // BaseCamp Additoon
+    public static class BaseCampActivity extends SettingsActivity {}
 }
