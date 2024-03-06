Colt-Enigma/platform_packages_apps_Settings

Settings: Let's separate BaseCamp
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -265,14 +265,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
             String key = preference.getKey();
             if (key.equals("top_level_network")
-            	|| key.equals("top_level_colt_settings")
+            	|| key.equals("top_level_display")
             	|| key.equals("top_level_apps")
             	|| key.equals("top_level_accessibility")
             	|| key.equals("top_level_emergency")
                 || key.equals("top_level_system")){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_top);
             } else if (key.equals("top_level_battery")
-            	|| key.equals("top_level_display")
             	|| key.equals("top_level_security")
             	|| key.equals("top_level_privacy")
             	|| key.equals("top_level_safety_center")
@@ -284,6 +283,8 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
             } else if (key.equals("top_level_accounts") && gAppsExists){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_middle);
+            } else if (key.equals("top_level_colt_settings")){
+                preference.setLayoutResource(R.layout.everestos_dashboard_preference_single);
             } else {
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
             }

ProjectEverest/android_packages_apps_Settings

src/com/android/settings/homepage/TopLevelSettings.java
@@ -265,14 +265,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
             String key = preference.getKey();
             if (key.equals("top_level_network")
-            	|| key.equals("top_level_base_camp")
+            	|| key.equals("top_level_display")
             	|| key.equals("top_level_apps")
             	|| key.equals("top_level_accessibility")
             	|| key.equals("top_level_emergency")
                 || key.equals("top_level_system")){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_top);
             } else if (key.equals("top_level_battery")
-            	|| key.equals("top_level_display")
             	|| key.equals("top_level_security")
             	|| key.equals("top_level_privacy")
             	|| key.equals("top_level_safety_center")
@@ -284,6 +283,8 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
             } else if (key.equals("top_level_accounts") && gAppsExists){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_middle);
+            } else if (key.equals("top_level_base_camp")){
+                preference.setLayoutResource(R.layout.everestos_dashboard_preference_single);
             } else {
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
             }