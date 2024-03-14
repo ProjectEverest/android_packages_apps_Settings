Havoc-Custom/packages_apps_settings

Settings: Redesign Settings Homepage
Co-authored-by: xyzuan <xyzuannihboss@gmail.com>
Co-authored-by: beingmishra <rahul731999@gmail.com>
Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -229,13 +229,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_display")
             	|| key.equals("top_level_apps")
             	|| key.equals("top_level_accessibility")
-            	|| key.equals("top_level_emergency")
-                || key.equals("top_level_system")){
+            	|| key.equals("top_level_emergency")){
                 preference.setLayoutResource(R.layout.superioros_dashboard_preference_top);
             } else if (key.equals("top_level_battery")
             	|| key.equals("top_level_security")
             	|| key.equals("top_level_privacy")
             	|| key.equals("top_level_storage")
+            	|| key.equals("top_level_system")
                 || key.equals("top_level_accounts")
                 || key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity")
             	|| key.equals("dashboard_tile_pref_com.google.android.apps.wellbeing.home.TopLevelSettingsActivity")
@@ -250,6 +250,8 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.superioros_dashboard_preference_middle);
             } else if (key.equals("top_level_lab")){
                 preference.setLayoutResource(R.layout.superioros_dashboard_preference_single);
+            } else if (key.equals("top_level_about_device")){
+                preference.setLayoutResource(R.layout.custom_dashboard_top);
             } else {
                 preference.setLayoutResource(R.layout.superioros_dashboard_preference_bottom);
             }

ProjectEverest/android_packages_apps_Settings

src/com/android/settings/homepage/TopLevelSettings.java
@@ -268,15 +268,15 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
             	|| key.equals("top_level_display")
             	|| key.equals("top_level_apps")
             	|| key.equals("top_level_accessibility")
-            	|| key.equals("top_level_emergency")
                 || key.equals("top_level_system")){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_top);
             } else if (key.equals("top_level_battery")
+              || key.equals("top_level_wallpaper")
             	|| key.equals("top_level_security")
             	|| key.equals("top_level_privacy")
             	|| key.equals("top_level_safety_center")
-            	|| key.equals("top_level_storage")
             	|| key.equals("top_level_wellbeing")
+            	|| key.equals("top_level_location")
             	|| key.equals("top_level_notifications")){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_middle);
             } else if ("top_level_google".equals(key)){
@@ -285,6 +285,8 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_middle);
             } else if (key.equals("top_level_base_camp")){
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_single);
+            } else if (key.equals("top_level_about_device")){
+                preference.setLayoutResource(R.layout.custom_dashboard_top);
             } else {
                 preference.setLayoutResource(R.layout.everestos_dashboard_preference_bottom);
             }